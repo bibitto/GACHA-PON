e2ea954fa157ef0b0f88abb9bda1632ef75b9dfb
bibitto <bibitto.crypto@gmail.com>
2022-12-24 20:50:47 +0900

    update: styles

---
+++ src/utils/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff',
+    teal: '',
+  },
+});
