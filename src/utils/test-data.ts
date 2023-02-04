80e3925daad46791c41083f62766eb150c0ce781
bibitto <bibitto.crypto@gmail.com>
2023-02-05 01:57:55 +0900

    test: add test nft data

---
+++ src/utils/test-data.ts
@@ -0,0 +1,58 @@
+export const testNfts = [
+  {
+    id: 1,
+    name: 'item1',
+    description:
+      'This is the first item. It is a unique and valuable item that is sure to impress anyone who sees it. Whether you are looking for a gift for a loved one or simply want to treat yourself, this item is sure to please.',
+    image: 'https://unsplash.it/800/1200?random',
+  },
+  {
+    id: 2,
+    name: 'item2',
+    description:
+      'This is the second item. It is made of high-quality materials and is built to last. It is versatile and can be used for a variety of purposes, making it a great addition to any collection. Whether you are a professional or a hobbyist, this item is sure to meet your needs.',
+    image: 'https://unsplash.it/700/700?random',
+  },
+  {
+    id: 3,
+    name: 'item3',
+    description:
+      'This is the third item. It is designed with both style and function in mind. It is perfect for those who value both form and function, and is sure to be a conversation piece wherever it is used. Whether you are looking for a statement piece or a functional item, this item is sure to please.',
+    image: 'https://unsplash.it/1000/800?random',
+  },
+  {
+    id: 4,
+    name: 'item4',
+    description:
+      'This is the fourth item. It is a rare and limited edition item that is sure to be a valuable addition to any collection. It is made of premium materials and is built to last. Whether you are a collector or simply looking for a unique and special item, this item is sure to impress.',
+    image: 'https://unsplash.it/1500/900?random',
+  },
+  {
+    id: 5,
+    name: 'item5',
+    description:
+      'This is the first item. It is a unique and valuable item that is sure to impress anyone who sees it. Whether you are looking for a gift for a loved one or simply want to treat yourself, this item is sure to please.',
+    image: 'https://unsplash.it/800/1200?random',
+  },
+  {
+    id: 6,
+    name: 'item6',
+    description:
+      'This is the second item. It is made of high-quality materials and is built to last. It is versatile and can be used for a variety of purposes, making it a great addition to any collection. Whether you are a professional or a hobbyist, this item is sure to meet your needs.',
+    image: 'https://unsplash.it/700/700?random',
+  },
+  {
+    id: 7,
+    name: 'item7',
+    description:
+      'This is the third item. It is designed with both style and function in mind. It is perfect for those who value both form and function, and is sure to be a conversation piece wherever it is used. Whether you are looking for a statement piece or a functional item, this item is sure to please.',
+    image: 'https://unsplash.it/1000/800?random',
+  },
+  {
+    id: 8,
+    name: 'item8',
+    description:
+      'This is the fourth item. It is a rare and limited edition item that is sure to be a valuable addition to any collection. It is made of premium materials and is built to last. Whether you are a collector or simply looking for a unique and special item, this item is sure to impress.',
+    image: 'https://unsplash.it/1500/900?random',
+  },
+];
