749a17214538bf1cf4cd60c3f4df14762da4a688
bibitto <bibitto.crypto@gmail.com>
2023-02-07 21:27:15 +0900

    test: add test NFTs

--- src/utils/test-data.ts
+++ src/utils/test-data.ts
@@ -32,27 +32,111 @@ export const testNfts = [
     name: 'item5',
     description:
       'This is the first item. It is a unique and valuable item that is sure to impress anyone who sees it. Whether you are looking for a gift for a loved one or simply want to treat yourself, this item is sure to please.',
-    image: 'https://unsplash.it/800/1200?random',
+    image: 'https://unsplash.it/800/1210?random',
   },
   {
     id: 6,
     name: 'item6',
     description:
       'This is the second item. It is made of high-quality materials and is built to last. It is versatile and can be used for a variety of purposes, making it a great addition to any collection. Whether you are a professional or a hobbyist, this item is sure to meet your needs.',
-    image: 'https://unsplash.it/700/700?random',
+    image: 'https://unsplash.it/700/690?random',
   },
   {
     id: 7,
     name: 'item7',
     description:
       'This is the third item. It is designed with both style and function in mind. It is perfect for those who value both form and function, and is sure to be a conversation piece wherever it is used. Whether you are looking for a statement piece or a functional item, this item is sure to please.',
-    image: 'https://unsplash.it/1000/800?random',
+    image: 'https://unsplash.it/1000/810?random',
   },
   {
     id: 8,
     name: 'item8',
     description:
       'This is the fourth item. It is a rare and limited edition item that is sure to be a valuable addition to any collection. It is made of premium materials and is built to last. Whether you are a collector or simply looking for a unique and special item, this item is sure to impress.',
-    image: 'https://unsplash.it/1500/900?random',
+    image: 'https://unsplash.it/1500/910?random',
+  },
+  {
+    id: 9,
+    name: 'item9',
+    description:
+      'This is the ninth item. It is a one-of-a-kind item that is sure to be a standout in any collection. It is expertly crafted and has a unique design that sets it apart from the rest. Whether you are looking for a unique and beautiful item or simply want to add something special to your collection, this item is sure to impress.',
+    image: 'https://unsplash.it/900/1000?random',
+  },
+  {
+    id: 10,
+    name: 'item10',
+    description:
+      'This is the tenth item. It is made from top-quality materials and is built to withstand the test of time. It is perfect for those who want a durable and long-lasting item that can be used for a variety of purposes. Whether you are a professional or simply looking for a reliable item, this item is sure to meet your needs.',
+    image: 'https://unsplash.it/1000/900?random',
+  },
+  {
+    id: 11,
+    name: 'item11',
+    description:
+      'This is the eleventh item. It is designed with elegance and sophistication in mind. It is perfect for those who appreciate fine design and want to add a touch of class to their collection. Whether you are looking for a statement piece or simply want to add something special to your space, this item is sure to please.',
+    image: 'https://unsplash.it/1100/900?random',
+  },
+  {
+    id: 12,
+    name: 'item12',
+    description:
+      'This is the twelfth item. It is a limited edition item that is sure to become a valuable part of any collection. It is made from the finest materials and is built to last. Whether you are a collector or simply looking for a unique and valuable item, this item is sure to impress.',
+    image: 'https://unsplash.it/1200/1000?random',
+  },
+  {
+    id: 13,
+    name: 'item13',
+    description:
+      'This is the thirteenth item. It is a one-of-a-kind item that is sure to be the center of attention in any collection. It is expertly crafted and has a unique design that sets it apart from the rest. Whether you are looking for a unique and beautiful item or simply want to add something special to your collection, this item is sure to impress.',
+    image: 'https://unsplash.it/1400/900?random',
+  },
+  {
+    id: 14,
+    name: 'item14',
+    description:
+      'This is the fourteenth item. It is made from high-quality materials and is built to last. It is versatile and can be used for a variety of purposes, making it a great addition to any collection. Whether you are a professional or a hobbyist, this item is sure to meet your needs.',
+    image: 'https://unsplash.it/1500/1000?random',
+  },
+  {
+    id: 15,
+    name: 'item15',
+    description:
+      'This is the fifteenth item. It is a luxurious item that is sure to turn heads and make a statement. It is made of the finest materials and is designed to be both stylish and functional. Whether you are looking to upgrade your collection or make a statement, this item is sure to impress.',
+    image: 'https://unsplash.it/1400/1100?random',
+  },
+  {
+    id: 16,
+    name: 'item16',
+    description:
+      'This is the sixteenth item. It is a unique and innovative item that is sure to stand out in any collection. It is made with cutting-edge technology and is designed to be both functional and visually stunning. Whether you are looking for something new and exciting or simply want to add something special to your collection, this item is sure to please.',
+    image: 'https://unsplash.it/1300/900?random',
+  },
+  {
+    id: 17,
+    name: 'item17',
+    description:
+      'This is the seventeenth item. It is a high-quality item that is designed to last. It is made with the best materials and is built to withstand daily use. Whether you are looking for a reliable item or simply want to add something of value to your collection, this item is sure to meet your needs.',
+    image: 'https://unsplash.it/1200/1200?random',
+  },
+  {
+    id: 18,
+    name: 'item18',
+    description:
+      'This is the eighteenth item. It is a beautifully designed item that is sure to add a touch of elegance to any collection. It is made with care and attention to detail, and is sure to impress anyone who sees it. Whether you are looking for a stunning item or simply want to add something special to your collection, this item is sure to please.',
+    image: 'https://unsplash.it/1100/1400?random',
+  },
+  {
+    id: 19,
+    name: 'item19',
+    description:
+      'This is the nineteenth item. It is a highly functional item that is designed to make your life easier. It is made with the latest technology and is built to meet the needs of even the most demanding users. Whether you are looking for a practical item or simply want to add something useful to your collection, this item is sure to impress.',
+    image: 'https://unsplash.it/900/1300?random',
+  },
+  {
+    id: 20,
+    name: 'item20',
+    description:
+      'This is the twentieth item. It is a rare and collectible item that is sure to be a valuable addition to any collection. It is made with the finest materials and is designed to last for generations. Whether you are a collector or simply looking for a unique and special item, this item is sure to impress.',
+    image: 'https://unsplash.it/1400/900?random',
   },
 ];
