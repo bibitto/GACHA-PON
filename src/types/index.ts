7094b38d227a8b79dd1d1c27bd2cbf8fbdc25a3d
bibitto <bibitto.crypto@gmail.com>
2023-02-22 21:40:43 +0900

    chore: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,45 @@ export type StatsCardProps = {
   unit: string;
   icon: ReactNode;
 };
+
+export type NFTMetadata = {
+  address: string;
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+};
+
+export type UploadMetadata = {
+  name: string;
+  description: string;
+  image: string;
+  external_url: string;
+};
+
+// export type FetchedNFT = {
+//   balance: string;
+//   contractAddress: string;
+//   decimals: string;
+//   name: string;
+//   symbol: string;
+//   type: string;
+// };
+
+export type Gacha = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  slashLink: string;
+  fee: number;
+  rootOwner: string;
+  isOpened: boolean;
+};
+
+export type Collection = {
+  address: string;
+  name: string;
+  symbol: string;
+  balance: string;
+};
